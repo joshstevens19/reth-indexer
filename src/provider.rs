52fa8ad02ba308abcd844af4dc952646af97d007
Josh Stevens <joshstevens19@hotmail.co.uk>
2023-07-11 18:39:13 +0100

    fix: reth_db::DatabaseEnvRO to make it future proof

--- src/provider.rs
+++ src/provider.rs
@@ -1,19 +1,16 @@
 use std::path::Path;
 
-use reth_db::{
-    mdbx::{Env, NoWriteMap},
-    open_db_read_only,
-};
+use reth_db::open_db_read_only;
 use reth_primitives::ChainSpecBuilder;
 use reth_provider::ProviderFactory;
 
-pub fn get_reth_factory(path: &Path) -> Result<ProviderFactory<Env<NoWriteMap>>, String> {
+pub fn get_reth_factory(path: &Path) -> Result<ProviderFactory<reth_db::DatabaseEnvRO>, String> {
     let db = open_db_read_only(path, None).map_err(|_| {
         "Could not open database make sure reth node is syncing and readable".to_string()
     })?;
 
     let spec = ChainSpecBuilder::mainnet().build();
-    let factory: ProviderFactory<Env<NoWriteMap>> = ProviderFactory::new(db, spec.into());
+    let factory: ProviderFactory<reth_db::DatabaseEnvRO> = ProviderFactory::new(db, spec.into());
 
     Ok(factory)
 }
